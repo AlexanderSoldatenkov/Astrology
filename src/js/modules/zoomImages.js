0f07e9df092b172484e8130a56fc4aa43f17e64f
Alexander <xspikyx@gmail.com>
2022-07-16 16:27:38 +0300

    .

--- src/js/modules/zoomImages.js
+++ src/js/modules/zoomImages.js
@@ -1,7 +1,9 @@
 const zoomImages = () => {
-  const imgPopup = document.createElement('div'),
-        workSection = document.querySelector('.certificates__wrapper'),
-        bigImage = document.createElement('img');
+  const imgPopup = document.createElement('div');
+  const imgBig = document.createElement('div');
+  const workSection = document.querySelector('.certificates__wrapper');
+  const scroll = calcScroll();
+ 
 
   imgPopup.classList.add('popup');
   workSection.appendChild(imgPopup);
@@ -10,25 +12,52 @@ const zoomImages = () => {
   imgPopup.style.alignItems = 'center';
   imgPopup.style.display = 'none';
 
-  imgPopup.appendChild(bigImage);
+  imgPopup.appendChild(imgBig);
 
   workSection.addEventListener('click', (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      let target = e.target;
-      //target.classList.contains('certificates__imgSmsll')
-    //target.querySelector('img')
-      if (target && target.classList.contains('certificates__imgSmall')) {
-          imgPopup.style.display = 'flex';
-          const path = target.parentNode.getAttribute('href');
-          bigImage.setAttribute('src', path);
-          console.log(target.nextElementSibling.innerHTML);
-      }
+    let target = e.target;
 
-      if (target && target.matches('div.popup')) {
-          imgPopup.style.display = 'none';
-      }
+    if (target && target.classList.contains('certificates__imgSmall')) {
+      imgPopup.style.display = 'flex';
+
+      document.body.style.overflow = "hidden";
+      document.body.style.marginRight = `${scroll}px`;
+
+      imgBig.innerHTML = target.nextElementSibling.innerHTML;
+
+      const close = imgBig.querySelector('.certificates__close');
+      close.addEventListener('click', (e) => {
+        e.preventDefault();
+        imgPopup.style.display = 'none';
+          document.body.style.overflow = "";
+          document.body.style.marginRight = `0px`;
+      });
+      // console.log(testEl);
+    }
+
+    if (target && target.matches('div.popup')) {
+      imgPopup.style.display = 'none';
+      document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
+    }
   });
+
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
 
 };
 
